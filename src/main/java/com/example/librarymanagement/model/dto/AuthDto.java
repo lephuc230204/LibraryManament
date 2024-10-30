f87e6b602693593bae33f37aff9c20d274dafe8d
lephuc230204 <lephuc04b3@gmail.com>
2024-10-30 22:07:39 +0700

    represent output data Authentication

---
+++ src/main/java/com/example/librarymanagement/model/dto/AuthDto.java
@@ -0,0 +1,29 @@
+package com.example.librarymanagement.model.dto;
+
+import com.example.librarymanagement.model.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthDto {
+    private Long id;
+    private String token;
+    private String refreshToken;
+    private String status;
+    private String result;
+
+    public static AuthDto from(User user, String token, String refreshToken, String status, String result) {
+        return AuthDto.builder()
+                .id(user.getId())
+                .token(token)
+                .refreshToken(refreshToken)
+                .status(status)
+                .result(result)
+                .build();
+    }
+}
