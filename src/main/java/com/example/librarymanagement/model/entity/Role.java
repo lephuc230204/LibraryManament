f3bc1d5fabb139c167cc6405f9bade45edb5b1b1
lephuc230204 <lephuc04b3@gmail.com>
2024-10-30 21:45:16 +0700

    Represent a Role enity

---
+++ src/main/java/com/example/librarymanagement/model/entity/Role.java
@@ -0,0 +1,28 @@
+package com.example.librarymanagement.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long RoleId;
+
+    @Column( unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+}
