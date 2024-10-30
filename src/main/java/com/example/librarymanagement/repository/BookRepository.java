6e45294f859622a7359302b8ecc80a77055c24f4
lephuc230204 <lephuc04b3@gmail.com>
2024-10-30 22:03:54 +0700

    Interacting db Book

---
+++ src/main/java/com/example/librarymanagement/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.librarymanagement.repository;
+
+import com.example.librarymanagement.model.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findById( Long bookId);
+}
