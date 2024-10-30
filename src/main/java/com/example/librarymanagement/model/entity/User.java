2b86e8204e1216fd8f3997d4135cb373c2426ecf
lephuc230204 <lephuc04b3@gmail.com>
2024-10-30 21:45:08 +0700

    Represent a user enity

---
+++ src/main/java/com/example/librarymanagement/model/entity/User.java
@@ -0,0 +1,82 @@
+package com.example.librarymanagement.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name ="Users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String phone;
+    private String address;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "Role_ID", nullable = false)
+    private com.example.librarymanagement.model.entity.Role role;
+
+    private LocalDate createdDate;
+    private String otpCode;
+    private LocalDate dob;
+
+    @Column(nullable = false)
+    private String status = "ACTIVE";
+
+    private String profilePicture;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getName() == null ? List.of() : List.of(new SimpleGrantedAuthority(role.getName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() { return username; }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    public Long getId() {
+        return userId;
+    }
+
+    public void setId(Long id) {
+        this.userId = id;
+    }
+}
