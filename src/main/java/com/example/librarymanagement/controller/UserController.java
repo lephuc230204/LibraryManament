a5c767196bd9f966dc9680d12e32d5d9ba717174
lephuc230204 <lephuc04b3@gmail.com>
2024-10-30 21:52:50 +0700

    Handle requests from users

---
+++ src/main/java/com/example/librarymanagement/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.librarymanagement.controller;
+
+import com.example.librarymanagement.model.dto.UserBasic;
+import com.example.librarymanagement.payload.request.ChangePasswordForm;
+import com.example.librarymanagement.payload.request.UserForm;
+import com.example.librarymanagement.payload.response.ResponseData;
+import com.example.librarymanagement.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@Validated
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    // Cập nhật thông tin cá nhân
+    @PutMapping("/update/me")
+    public ResponseEntity<ResponseData<String>> updateCurrentUser(@RequestBody UserForm form, Principal principal) {return ResponseEntity.ok(userService.updateMe(principal, form));
+    }
+
+    // Cập nhật mật khẩu
+    @PatchMapping("/change-password")
+    public ResponseEntity<ResponseData<String> > changePassword(@RequestBody ChangePasswordForm form, Principal principal) {return ResponseEntity.ok(userService.changePassword(form, principal));
+    }
+
+    // Lấy thông tin người dùng hiện tại
+    @GetMapping("/me")
+    public ResponseEntity<ResponseData<UserBasic> > getCurrentUser(Principal principal) {return ResponseEntity.ok(userService.getMe(principal));
+    }
+
+}
