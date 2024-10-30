abc90cbe800ef805db0ee9c98bfcfea614266601
lephuc230204 <lephuc04b3@gmail.com>
2024-10-30 21:53:24 +0700

    Provide data Spring security

---
+++ src/main/java/com/example/librarymanagement/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.librarymanagement.service.impl;
+
+import com.example.librarymanagement.model.entity.User;
+import com.example.librarymanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Tìm người dùng dựa trên email
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email: " + email));
+
+        // Trả về một đối tượng UserDetails từ Spring Security
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRole().getName())
+                .build();
+    }
+}
